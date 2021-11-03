cf93212f1ea73fb5287966c0ef9482e3c6dd0ba8
João Victor S. P <victoronstudy@gmail.com>
2021-11-03 15:24:41 -0300

    Adiciona testes para letras com acentos portugueses

--- tests.py
+++ tests.py
@@ -32,6 +32,36 @@ def testar_revelar_letra():
         tem_teste_funcionando = False
         print("Erro na função revelar_letra, teste 2.")
 
+    palavra_teste_3 = "lâmpada"
+    letra_3 = "a"
+    palavra_ocultada_3 = "_" * len(palavra_teste_3)
+    palavra_revelada_3 = revelar_letra(
+            letra_3,
+            palavra_ocultada_3,
+            palavra_teste_3
+            )
+    retorno_esperado_3 = "_â__a_a"
+
+    tem_retorno_correto_3 = palavra_revelada_3 == retorno_esperado_3
+    if not tem_retorno_correto_3:
+        tem_teste_funcionando = False
+        print("Erro na função revelar_letra, teste 3.")
+
+    palavra_teste_4 = "açúcar"
+    letra_4 = "c"
+    palavra_ocultada_4 = "_" * len(palavra_teste_4)
+    palavra_revelada_4 = revelar_letra(
+            letra_4,
+            palavra_ocultada_4,
+            palavra_teste_4
+            )
+    retorno_esperado_4 = "_ç_c__"
+
+    tem_retorno_correto_4 = palavra_revelada_4 == retorno_esperado_4
+    if not tem_retorno_correto_4:
+        tem_teste_funcionando = False
+        print("Erro na função revelar_letra, teste 4.")
+
     if tem_teste_funcionando:
         print("Passou nos testes da função revelar_letra.")
 
