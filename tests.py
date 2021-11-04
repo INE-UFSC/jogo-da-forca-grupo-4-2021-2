36c0cb8c12a9b249f4fa407bd1354742f00ca6df
João Victor S. P <victoronstudy@gmail.com>
2021-11-04 14:57:44 -0300

    Mostra detalhes de cada caso teste

--- tests.py
+++ tests.py
@@ -12,6 +12,11 @@ def testar_revelar_letra():
     palavra_revelada = revelar_letra(letra, palavra_ocultada, palavra_teste)
     retorno_esperado = "_e__e"
 
+    resumo_do_teste_1 = ("\nTestando revelar_letra:\nPalavra teste 1: "
+            + palavra_teste + ", Letra: " + letra + ", Palavra revelada 1: "
+            + palavra_revelada)
+    print(resumo_do_teste_1)
+
     tem_retorno_correto = palavra_revelada == retorno_esperado
     if not tem_retorno_correto:
         tem_teste_funcionando = False
@@ -26,6 +31,11 @@ def testar_revelar_letra():
             palavra_teste_2
             )
     retorno_esperado_2 = palavra_ocultada_2
+
+    resumo_do_teste_2 = ("\nTestando revelar_letra:\nPalavra teste 2: "
+            + palavra_teste_2 + ", Letra: " + letra_2
+            + ", Palavra revelada 2: " + palavra_revelada_2)
+    print(resumo_do_teste_2)
 
     tem_retorno_correto_2 = palavra_revelada_2 == retorno_esperado_2
     if not tem_retorno_correto_2:
@@ -42,6 +52,11 @@ def testar_revelar_letra():
             )
     retorno_esperado_3 = "_â__a_a"
 
+    resumo_do_teste_3 = ("\nTestando revelar_letra:\nPalavra teste 3: "
+            + palavra_teste_3 + ", Letra: " + letra_3
+            + ", Palavra revelada 3: " + palavra_revelada_3)
+    print(resumo_do_teste_3)
+
     tem_retorno_correto_3 = palavra_revelada_3 == retorno_esperado_3
     if not tem_retorno_correto_3:
         tem_teste_funcionando = False
@@ -56,6 +71,11 @@ def testar_revelar_letra():
             palavra_teste_4
             )
     retorno_esperado_4 = "_ç_c__"
+
+    resumo_do_teste_4 = ("\nTestando revelar_letra:\nPalavra teste 4: "
+            + palavra_teste_4 + ", Letra: " + letra_4
+            + ", Palavra revelada 4: " + palavra_revelada_4)
+    print(resumo_do_teste_4)
 
     tem_retorno_correto_4 = palavra_revelada_4 == retorno_esperado_4
     if not tem_retorno_correto_4:
